d22d2c37e3e86d03dcb41d0d55f9a30c078ac728
Marshall Chan <marshallchan666@outlook.com>
2022-02-10 06:27:30 +0800

    shadow intro

--- src/shadow.js
+++ src/shadow.js
@@ -16,10 +16,18 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
+ *  Texture
+ */
+const textureLoader = new THREE.TextureLoader();
+const simpleShadow = textureLoader.load(
+  "/textures/shadow/simpleShadow.jpg"
+);
+
+/**
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 gui
   .add(ambientLight, "intensity")
   .min(0)
@@ -30,9 +38,23 @@ scene.add(ambientLight);
 // Directional light
 const directionalLight = new THREE.DirectionalLight(
   0xffffff,
-  0.5
+  0.3
 );
 directionalLight.position.set(2, 2, -1);
+directionalLight.castShadow = true;
+
+// * shadow config combinations
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 6;
+
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.right = 2;
+directionalLight.shadow.camera.bottom = -2;
+directionalLight.shadow.camera.left = -2;
+// directionalLight.shadow.radius = 10;
+
 gui
   .add(directionalLight, "intensity")
   .min(0)
@@ -55,6 +77,44 @@ gui
   .step(0.001);
 scene.add(directionalLight);
 
+// spot light
+const spotLight = new THREE.SpotLight(
+  0xffffff,
+  0.3,
+  10,
+  Math.PI * 0.3
+);
+
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.fov = 30;
+spotLight.shadow.camera.near = 1;
+spotLight.shadow.camera.far = 6;
+
+spotLight.position.set(0, 2, 2);
+
+scene.add(spotLight);
+scene.add(spotLight.target);
+
+// point light
+const pointLight = new THREE.PointLight(0xffffff, 0.3);
+
+pointLight.castShadow = true;
+
+pointLight.position.set(-1, 1, 0);
+scene.add(pointLight);
+pointLight.shadow.mapSize.width = 1024;
+pointLight.shadow.mapSize.height = 1024;
+
+pointLight.shadow.camera.near = 0.1;
+pointLight.shadow.camera.far = 5;
+const pointLightCameraHelper = new THREE.CameraHelper(
+  pointLight.shadow.camera
+);
+pointLightCameraHelper.visible = false;
+scene.add(pointLightCameraHelper);
+
 /**
  *  Light helper
  */
@@ -62,6 +122,22 @@ const directionalLightHelper =
   new THREE.DirectionalLightHelper(directionalLight);
 scene.add(directionalLightHelper);
 
+const directionalLightCameraHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera
+);
+directionalLightCameraHelper.visible = false;
+scene.add(directionalLightCameraHelper);
+
+const spotLightHelper = new THREE.SpotLightHelper(
+  spotLight
+);
+scene.add(spotLightHelper);
+
+const spotLightCameraHelper = new THREE.CameraHelper(
+  spotLight.shadow.camera
+);
+spotLightCameraHelper.visible = false;
+scene.add(spotLightCameraHelper);
 /**
  * Materials
  */
@@ -77,6 +153,7 @@ const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(0.5, 32, 32),
   material
 );
+sphere.castShadow = true;
 
 const plane = new THREE.Mesh(
   new THREE.PlaneGeometry(5, 5),
@@ -84,9 +161,21 @@ const plane = new THREE.Mesh(
 );
 plane.rotation.x = -Math.PI * 0.5;
 plane.position.y = -0.5;
+plane.receiveShadow = true;
 
 scene.add(sphere, plane);
 
+const sphereShadow = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(1.5, 1.5),
+  new THREE.MeshBasicMaterial({
+    color: 0x000,
+    transparent: true,
+    alphaMap: simpleShadow,
+  })
+);
+sphereShadow.rotation.x = -Math.PI / 2;
+sphereShadow.position.y = plane.position.y + 0.01;
+scene.add(sphereShadow);
 /**
  * Sizes
  */
@@ -141,8 +230,8 @@ renderer.setPixelRatio(
   Math.min(window.devicePixelRatio, 2)
 );
 
-renderer.shadowMap.enabled = true;
-
+renderer.shadowMap.enabled = false;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 /**
  * Animate
  */
@@ -151,6 +240,17 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
+  // update Sphere
+  sphere.position.x = Math.cos(elapsedTime) * 1.5;
+  sphere.position.z = Math.sin(elapsedTime) * 1.5;
+  sphere.position.y = Math.abs(
+    Math.sin(elapsedTime * 3) * 1.5
+  );
+
+  // update the shadow
+  sphereShadow.position.x = sphere.position.x;
+  sphereShadow.position.z = sphere.position.z;
+  sphereShadow.material.opacity = sphere.position.y * 0.3;
   // Update controls
   controls.update();
 
