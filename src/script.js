4fdef3807beb5e44a6eb353807ca43f041712644
Marshall Chan <marshallchan666@outlook.com>
2022-02-10 04:56:10 +0800

    shadow started

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 // import textures from "./textures";
 // import materials from "./materials";
 // import text from "./text3D";
-import light from "./light";
+// import light from "./light";
+import shadow from "./shadow";
