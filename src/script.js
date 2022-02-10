948256a6982863790cff2519f9dd8b15857e5099
Marshall Chan <marshallchan666@outlook.com>
2022-02-11 05:42:23 +0800

    amazing particles

--- src/script.js
+++ src/script.js
@@ -3,4 +3,5 @@
 // import text from "./text3D";
 // import light from "./light";
 // import shadow from "./shadow";
-import hauntedHouse from "./hauntedHouse";
+// import hauntedHouse from "./hauntedHouse";
+import particles from "./particle";
